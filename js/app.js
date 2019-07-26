10032877212543d86bff92b3d0253825f4ffb2b2
JangJur <wkdwndud753@naver.com>
2019-07-26 16:05:33 +0900

    Event Bus2

--- js/app.js
+++ js/app.js
@@ -1,47 +1,18 @@
-Vue.component('todo-footer', {
-    props: ['propsdata'],
-    template: '<p>{{ propsdata }}</p>'
-});
-
-var cmp1 = {
-    template: '<div>This is another local child component1 <br> : {{ cmp1Data }}'
-        + '<br><button v-on:click="showLog">show</button></div>',
+var eventBus = new Vue();
+Vue.component('child-component', {
+    template: '<div>하위 컴포넌트 영역입니다.<button v-on:click="showLog">show</button></div>',
     methods: {
-        showLog: function () {
-            this.$emit('show-log');
-        }
-    },
-    data: function () {
-        return {
-            cmp1Data: 100
+        showLog: function() {
+            eventBus.$emit('triggerEventBus', 100);
         }
     }
-};
-
-var cmp2 = {
-    template: '<div>This is another local child component2 <br> : {{ cmp2Data }}</div>',
-    data: function () {
-        return {
-            cmp2Data: cmp1.data.cmp1Data
-        }
-    }
-};
+});
 
 var app = new Vue({
     el: '#app',
-    data: {
-        props_msg: 'Hello Vue! passed from Parent Component'
-    },
-
-    components: {
-        'todo-list1': cmp1,
-        'todo-list2': cmp2
-    },
-
-    methods: {
-        printText: function () {
-            console.log("received an event");
-        }
+    created: function() {
+        eventBus.$on('triggerEventBus', function(value) {
+            console.log("이벤트를 전달받음. 전달받은 값 : ", value);
+        });
     }
-
 });
