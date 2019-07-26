ba2a10e1610d27988ad249c36353101922dd8fcf
JangJur <wkdwndud753@naver.com>
2019-07-26 17:13:46 +0900

    quiz/04-1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 var Body = { template: '<div>This is Body</div>' };
 var Header = { template: '<div>This is Header</div>' };
-var Footer = { template: '<div>This is Footer</div>' };
+var Footer = { template: '<div>This issss Footer</div>' };
+
+var LoginBody = { template: '<div>This is LoginBody</div>' };
+var LoginHeader = { template: '<div>This is LoginHeader</div>' };
+var LoginFooter = { template: '<div>This is LoginsFooter</div>' };
 
 var router = new VueRouter({
     routes: [
@@ -10,6 +14,14 @@ var router = new VueRouter({
                 default: Body,
                 header: Header,
                 footer: Footer
+            }
+        },
+        {
+            path: '/login',
+            components: {
+                default: LoginBody,
+                header: LoginHeader,
+                footer: LoginFooter
             }
         }
     ]
