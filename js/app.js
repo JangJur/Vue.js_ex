f53533e562ef79c0f99b6a0cf532862f2d1547fb
JangJur <wkdwndud753@naver.com>
2019-07-26 17:04:12 +0900

    Nested Router

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
-var Main = { template: '<div>main</div>' };
-var Login = { template: '<div>login</div>' };
+var User = {
+    template: `
+    <div>
+        User Component
+        <router-view></router-view>
+    </div>
+    `
+};
+var UserProfile = { template: '<p>User Profile Component</p>' };
+var UserPost = { template: '<p>User Post Component</p>' };
 
 var routes = [
-    { path: '/main', component: Main },
-    { path: '/login', component: Login }
+    {
+        path: '/user',
+        component: User,
+        children: [
+            {
+                path: 'posts',
+                component: UserPost
+            },
+            {
+                path: 'profile',
+                component: UserProfile
+            },
+        ]
+
+    }
 ];
 
 var router = new VueRouter({
-    mode: 'history',
     routes
 });
 
