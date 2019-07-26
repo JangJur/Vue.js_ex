5a53ecba1e46f995c68f75a5f77af34591ac3a04
JangJur <wkdwndud753@naver.com>
2019-07-26 16:28:23 +0900

    quiz/03-3

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-var eventBus = new Vue();
+Vue.component('slibling-component', {
+    props: ['slib'],
+    template: '<p>{{ slib }}</p>'
+});
+
 Vue.component('child-component', {
-    template: '<div>하위 컴포넌트 영역입니다.<button v-on:click="showLog">show</button></div>',
-    methods: {
-        showLog: function() {
-            eventBus.$emit('triggerEventBus', 100);
-        }
-    }
+    props: ['propsdata'],
+    template: '<p>{{ propsdata }}</p>'
 });
 
 var app = new Vue({
     el: '#app',
-    created: function() {
-        eventBus.$on('triggerEventBus', function(value) {
-            console.log("이벤트를 전달받음. 전달받은 값 : ", value);
-        });
+    data: {
+        message: 'Hello Vue! passed from Parent Component',
+        anotherMessage : 'You know what? This is another message!'
     }
+
 });
