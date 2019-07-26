08fcaedf338fb83e7f09f68f98b28ae13479788d
JangJur <wkdwndud753@naver.com>
2019-07-26 17:48:42 +0900

    Axios

--- js/app.js
+++ js/app.js
@@ -2,10 +2,9 @@ new Vue({
     el: '#app',
     methods: {
         getData: function() {
-            this.$http.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
+            axios.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
             .then(function(response){
                 console.log(response);
-                console.log(JSON.parse(response.data));
             });
         }
     }
