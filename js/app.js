531de5320f6d69fde548799ee2faab2ea87a89f9
JangJur <wkdwndud753@naver.com>
2019-07-26 14:08:54 +0900

    Event Bus

--- js/app.js
+++ js/app.js
@@ -2,22 +2,27 @@ Vue.component('todo-footer', {
     props: ['propsdata'],
     template: '<p>{{ propsdata }}</p>'
 });
-var cmp1Data;
 
 var cmp1 = {
-    template: '<div>This is another local child component1 <br> : {{ cmp1Data }}</div>',
-    data: function() {
+    template: '<div>This is another local child component1 <br> : {{ cmp1Data }}'
+        + '<br><button v-on:click="showLog">show</button></div>',
+    methods: {
+        showLog: function () {
+            this.$emit('show-log');
+        }
+    },
+    data: function () {
         return {
-            cmp1Data : 100
+            cmp1Data: 100
         }
     }
 };
 
 var cmp2 = {
     template: '<div>This is another local child component2 <br> : {{ cmp2Data }}</div>',
-    data: function() {
+    data: function () {
         return {
-            cmp2Data : cmp1.data.cmp1Data
+            cmp2Data: cmp1.data.cmp1Data
         }
     }
 };
@@ -32,5 +37,11 @@ var app = new Vue({
         'todo-list1': cmp1,
         'todo-list2': cmp2
     },
+
+    methods: {
+        printText: function () {
+            console.log("received an event");
+        }
+    }
 
 });
